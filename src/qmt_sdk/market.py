1d3360026d9b1bba4e91691d064fc5dd5a5d86ad
虞力 <humbert.yu@outlook.com>
2026-09-02 17:50:28 +0800

    Expose QMT subscription and formula client methods

--- src/qmt_sdk/market.py
+++ src/qmt_sdk/market.py
@@ -26,11 +26,35 @@ class MarketClient:
                                   start_time, end_time, count, dividend_type,
                                   fill_data, data_dir)
 
-    def subscribe_quote(self, *args, **kwargs):
-        return self._backend.call("subscribe_quote", *args, **kwargs)
+    def subscribe_quote(self, stock_code, period="tick", start_time="", end_time="",
+                        count=0, callback=None):
+        return self._backend.call("subscribe_quote", stock_code, period, start_time,
+                                  end_time, count, callback)
 
-    def unsubscribe_quote(self, *args, **kwargs):
-        return self._backend.call("unsubscribe_quote", *args, **kwargs)
+    def subscribe_whole_quote(self, code_list, callback=None):
+        return self._backend.call("subscribe_whole_quote", code_list, callback)
+
+    def subscribe_formula(self, formula_name, stock_code, period, start_time="",
+                          end_time="", count=-1, dividend_type=None,
+                          extend_param=None, callback=None):
+        return self._backend.call("subscribe_formula", formula_name, stock_code,
+                                  period, start_time, end_time, count,
+                                  dividend_type, extend_param or {}, callback)
+
+    def unsubscribe_quote(self, subscription_id):
+        return self._backend.call("unsubscribe_quote", subscription_id)
+
+    def unsubscribe_formula(self, request_id):
+        return self._backend.call("unsubscribe_formula", request_id)
+
+    def call_formula(self, formula_name, stock_code, period, start_time="",
+                     end_time="", count=-1, dividend_type=None, extend_param=None):
+        return self._backend.call("call_formula", formula_name, stock_code, period,
+                                  start_time, end_time, count, dividend_type,
+                                  extend_param or {})
+
+    def call_formula_batch(self, *args, **kwargs):
+        return self._backend.call("call_formula_batch", *args, **kwargs)
 
     def download_history_data2(self, *args, **kwargs):
         return self._backend.call("download_history_data2", *args, **kwargs)
